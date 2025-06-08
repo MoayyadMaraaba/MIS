ab86aa6e06e8ce9afbd89e6846d5552c3d4c11e1
tasneem1010 <tasneemabusra@gmail.com>
2025-06-08 23:49:08 +0300

    Added Case Archive

--- models/Case.py
+++ models/Case.py
@@ -6,6 +6,8 @@ from pydantic import BaseModel
 from models.Incident import location
 from models.Incident import evidence
 
+
+# Enum for case status.
 class CaseStatus(str, Enum):
     """
     Enum for case status.
@@ -15,12 +17,16 @@ class CaseStatus(str, Enum):
     RESOLVED = "resolved"
     ARCHIVED = "archived"
 
+
+# Enum for case priority.
 class Priority(str, Enum):
     """Enum for case priority.
     """
     LOW = "low"
     MEDIUM = "medium"
     HIGH = "high"   
+
+# A perpetrator in the system.
 class Perpetrator(BaseModel):
     name:str
     type:str
@@ -29,18 +35,19 @@ class Case(BaseModel):
     """
     Represents a case in the system.
     """
-    case_id: str
-    title: str
+    case_id: str # Unique identifier for the case
+    title: str # Title of the case
     description: str = ""  # Default description is an empty string
     violation_types: list[str]  # List of violation types
     status: CaseStatus  # Using the CaseStatus Enum
     priority: Priority  # Using the Priority Enum
-    location: location
+    location: location # Location of the incident
+    date_occurred: datetime  # Date when the incident occurred
+    date_reported: datetime # Date when the case was reported
     victims: list[str]  # List of victim IDs
-    evidence: list[evidence]
+    evidence: list[evidence] # List of evidence items
     perpetrators: list[Perpetrator]  # List of perpetrator IDs
-
     created_by: str  # ID of the user who created the case
-    created_at: datetime = datetime.now()
-    updated_at: datetime = datetime.now()
+    created_at: datetime = datetime.now() # Timestamp when the case was created
+    updated_at: datetime = datetime.now() # Timestamp when the case was last updated
 
--- routes/Cases.py
+++ routes/Cases.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Query,status,Body,UploadFile,File,Form,Path
+from fastapi import APIRouter, Query,status,Body,UploadFile,File,Form,Path,Header
 import bcrypt
 from utils.helpers import res
 from datetime import datetime, timedelta, timezone
@@ -10,6 +10,7 @@ from random import randint
 import shutil
 import json
 from models.Case import Case, CaseStatus
+import utils.helpers as helpers
 
 load_dotenv()
 
@@ -27,8 +28,8 @@ db = createConnection()
 # • GET /cases/ – List all cases - search/filter functionality (by date, location, violation type). ✅
 # • POST /cases/ – Create a new case ✅
 # • GET /cases/{case_id} – Retrieve a specific case ✅
-# • PATCH /cases/{case_id} – Update case status ✅ TODO save to case_status_history
-# • DELETE /cases/{case_id} – Archive a case 
+# • PATCH /cases/{case_id} – Update case status + save to case_status_history ✅ 
+# • DELETE /cases/{case_id} – Archive a case + Save to Cases_Archive ✅
 
 # List all cases - search/filter functionality (by date, location (city), violation type).
 @router.get("/")
@@ -77,31 +78,70 @@ def create_case(case: Case):
 
     return res(status_code=status.HTTP_201_CREATED, content="Case created successfully, with id: " + str(result.inserted_id))
 
-# TODO save to case_status_history
 # Update case status
 @router.patch("/{case_id}")
-def update_case_status(case_id: str, caseStatus: CaseStatus):
+def update_case_status(case_id: str, caseStatus: CaseStatus, authorization: str = Header(..., alias="Authorization")):
     # Validate the case ID
     if not db.cases.find_one({"case_id": case_id}):
         return res(status_code=status.HTTP_404_NOT_FOUND, content="Case not found.")
-
+    
     # Update the case status
     result = db.cases.update_one({"case_id": case_id}, {"$set": {"status": caseStatus}})
-
+    
+    # Get the token from the authorization header
+    token = authorization.split(" ")[0]
+    # Get the payload from the JWT token 
+    # Admin is the only role that can update case status
+    payload = helpers.check_jwt(token, "Admin")
+    # Add status update to case_status_history
+    result_history = db.case_status_history.insert_one({
+        "case_id": case_id,
+        "status": caseStatus,
+        "updated_at": datetime.now(timezone.utc),
+        "updated_by": payload["userId"] 
+    })
+    
     if result.modified_count == 0:
         return res(status_code=status.HTTP_400_BAD_REQUEST, content="Failed to update case status.")
-
+    if result_history.inserted_id is None:
+        return res(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content="Failed to save case status history.")
+        
     return res(status_code=status.HTTP_200_OK, content="Case status updated successfully.")
 
-# TODO Save to Cases_Archive
 # Archive a case 
 # Delete case
 @router.delete("/{case_id}")
-def delete_case(case_id: str):
-    # Validate the case ID
-    if not db.cases.find_one({"case_id": case_id}):
-        return res(status_code=status.HTTP_404_NOT_FOUND, content="Case not found.")
+def delete_case(case_id: str, authorization: str = Header(..., alias="Authorization")):
 
+    # Validate the case ID
+    case = db.cases.find_one({"case_id": case_id})
+    if not case:
+        return res(status_code=status.HTTP_404_NOT_FOUND, content="Case not found.")
+    
+    token = authorization.split(" ")[0]
+    payload = helpers.check_jwt(token, "Admin")
+    
+    # Archive the case
+    db.cases_archive.insert_one({
+        "case_id": case["case_id"],
+        "title": case["title"],
+        "description": case["description"],
+        "violation_types": case["violation_types"],
+        "status": case["status"],
+        "priority": case["priority"],
+        "location": case["location"],
+        "date_occurred": case["date_occurred"],
+        "date_reported": case["date_reported"],
+        "victims": case["victims"],
+        "evidence": case["evidence"],
+        "perpetrators": case["perpetrators"],
+        "created_by": case["created_by"],
+        "created_at": case["created_at"],
+        "updated_at": case["updated_at"],
+        "archived_at": datetime.now(timezone.utc),
+        # Get userId from the JWT token
+        "archived_by": payload["userId"]})
+    
     # Delete the case
     result = db.cases.delete_one({"case_id": case_id})
 
